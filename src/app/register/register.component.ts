1728364134ba8c6d60cfd45b84bb5b7cff703202
codeconcept <contact@codeconcept.fr>
2020-06-11 19:34:09 +0200

    Logging in

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -10,12 +10,19 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
+  loginForm: FormGroup;
   result;
+  user;
 
   constructor(private fb: FormBuilder, private afAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
+      email: ['', Validators.email],
+      password: ['',[Validators.required, Validators.minLength(6)]]
+    });
+
+    this.loginForm = this.fb.group({
       email: ['', Validators.email],
       password: ['',[Validators.required, Validators.minLength(6)]]
     });
@@ -30,6 +37,16 @@ export class RegisterComponent implements OnInit {
     console.log('register', this.registerForm.value); 
     this.result = await this.afAuth.createUserWithEmailAndPassword(this.registerForm.value.email,this.registerForm.value.password);
     this.registerForm.reset();
+  }
+
+  async login() {
+    if (!this.loginForm.valid) {
+      console.log(':(');
+      return;
+    }
+    console.log('login', this.loginForm.value);
+    const { email, password} = this.loginForm.value;
+    this.user = await this.afAuth.signInWithEmailAndPassword(email, password);
   }
 
 }
