f0b427ad87e59a55f983e9db6b9a64837e7c9f22
codeconcept <contact@codeconcept.fr>
2020-06-17 20:28:20 +0200

    First HTTP cloud function + log in online journal

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+    console.log(`Logged at ${new Date().toISOString()}`);
+    response.send("Hello from Firebase!");
+});
